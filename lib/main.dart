83e05b3ac5764ce1c8fcd4c3e9fc2618f4179af1
Julien Baron <julien.baron@my-digital-school.org>
2023-06-12 10:11:18 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const calculator());
+}
+
+class calculator extends StatelessWidget {
+  const calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Calculatrice",
+      theme: ThemeData(primarySwatch: blue),
+      home: 
+    );
+  }
+}
